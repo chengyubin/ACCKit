9409f3cda341011c3a64e9c575dbc0d6e825fc8c
CCyber <371247200@qq.com>
2021-01-25 15:14:02 +0800

    Initial commit

---
+++ Example/ACCKit/ACCViewController.m
@@ -0,0 +1,29 @@
+//
+//  ACCViewController.m
+//  ACCKit
+//
+//  Created by chengyb on 01/25/2021.
+//  Copyright (c) 2021 chengyb. All rights reserved.
+//
+
+#import "ACCViewController.h"
+
+@interface ACCViewController ()
+
+@end
+
+@implementation ACCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Example/ACCKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ACCKit
+//
+//  Created by chengyb on 01/25/2021.
+//  Copyright (c) 2021 chengyb. All rights reserved.
+//
+
+@import UIKit;
+#import "ACCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ACCAppDelegate class]));
+    }
+}
